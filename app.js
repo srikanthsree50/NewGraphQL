e117d5fbb91c64ff86e3f08eb8be34b23311ca10
srikanth <srikanthsree50@gmail.com>
2019-09-10 22:06:06 +0530

    mvc commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const errorController = require('./controllers/error')
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -8,17 +8,15 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.getError);
 
 app.listen(3000);
