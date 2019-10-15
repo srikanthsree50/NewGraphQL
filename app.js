93f6f76d98803241adc419251526daa8872b1a2a
srikanth <srikanthsree50@gmail.com>
2019-10-15 21:21:09 +0530

    error handling

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const flash = require('connect-flash');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI =
+const MONGODB_URI = 
   'mongodb+srv://newUser:newUser@clusterone-plhk1.mongodb.net/Shop?retryWrites=true&w=majority';
 
 const app = express();
@@ -47,10 +47,15 @@ app.use((req, res, next) => {
   }
   User.findById(req.session.user._id)
     .then(user => {
+      if (!user) {
+        return next();
+      }
       req.user = user;
       next();
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      throw new Error(err);
+    });
 });
 
 app.use((req, res, next) => {
@@ -63,7 +68,14 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.get('/500', errorController.get500);
+
 app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  // res.status(error.httpStatusCode).render(...);
+  res.redirect('/500');
+});
 
 mongoose
   .connect(MONGODB_URI,{useNewUrlParser:true,useUnifiedTopology:true})
